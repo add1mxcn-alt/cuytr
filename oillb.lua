3e360a2b6e1a3b217ceb80b30cb8ce4801686b42
add1mxcn-alt <add1mxcn@gmail.com>
2026-08-02 15:49:47 +0300

    Update oillb.lua

--- oillb.lua
+++ oillb.lua
@@ -1,18 +1,1 @@
-local MilitarySelected = nil
-
-local MilitaryDropdown = TrollTab:AddDropdown({
-    Name = "اختيار اللاعب",
-    Default = "",
-    Multi = false,
-    Options = GetPlayerNames(),
-    Callback = function(name)
-        MilitarySelected = Players:FindFirstChild(name)
-    end
-})
-
-TrollTab:AddButton({
-    Name = "تحديث قائمة اللاعبين",
-    Callback = function()
-        MilitaryDropdown:Set(GetPlayerNames())
-    end
-})
+تتتتاااا
